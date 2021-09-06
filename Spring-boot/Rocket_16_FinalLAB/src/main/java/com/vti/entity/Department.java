d101705fd71e37d2bf7dfe1a2098ae03a0e0d98c
doduchung1999 <86633597+doduchung1999@users.noreply.github.com>
2021-09-06 18:38:43 +0700

    FinalLAB

---
+++ Spring-boot/Rocket_16_FinalLAB/src/main/java/com/vti/entity/Department.java
@@ -0,0 +1,66 @@
+package com.vti.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Table(name = "Department", catalog = "TestingSystem")
+public class Department implements Serializable {
+	@Column(name = "DepartmentID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private short id;
+
+	@Column(name = "DepartmentName", length = 30, nullable = false, unique = true)
+	private String name;
+
+	@OneToMany(mappedBy = "department", fetch = FetchType.EAGER)
+	@Cascade(value = { CascadeType.REMOVE, CascadeType.SAVE_UPDATE })
+	private List<Account> accounts;
+
+	public Department() {
+		super();
+	}
+
+	public short getId() {
+		return id;
+	}
+
+	public void setId(short id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ Spring-boot/Rocket_16_FinalLAB/src/main/java/com/vti/service/IPositionService.java
@@ -0,0 +1,10 @@
+package com.vti.service;
+
+import java.util.List;
+
+import com.vti.entity.Position;
+
+public interface IPositionService {
+	public List<Position> getAllPositions();
+	
+}
